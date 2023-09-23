9df0e4e42f0bbccb4e94f79dbed8a0b2b3aa5bbc
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2023-09-23 11:42:50 +0600

    feat: Add EnergyUsage Model for Logs energy consumption data for devices, allowing users to monitor and track energy usage.

---
+++ Homee.Models/EnergyUsage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homee.Models;
+
+public class EnergyUsage
+{
+    [Key]
+    public int Id { get; set; }
+    public int DeviceId { get; set; }
+    [ForeignKey("DeviceId")]
+    public Device Device { get; set; }
+    public DateTime Timestamp { get; set; }
+    public decimal Consumption { get; set; }
+}

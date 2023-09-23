dea63f72db7add4ce10c1b7d5a571d3a604be6bc
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2023-09-23 15:24:38 +0600

    feat: Add Application User to the Db

--- Homee.DataAccess/Data/AppDbContext.cs
+++ Homee.DataAccess/Data/AppDbContext.cs
@@ -1,20 +1,14 @@
-﻿using Homee.Models;
-using Homee.Models.Auth;
+﻿using Homee.Models.Auth;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Homee.DataAccess.Data;
 
-public class AppDbContext : IdentityDbContext
+public class AppDbContext : IdentityDbContext<ApplicationUser>
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
         
     }
-    //public DbSet<Device> Devices { get; set; }
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 }

608654009072867149f63c9c85d27e46e3847d8d
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2023-09-23 11:41:08 +0600

    feat: Add Event Table. Records events generated by devices or user actions, which can be used for automation triggers and historical analysis.

---
+++ Homee.Models/Event.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homee.Models;
+
+public class Event
+{
+    [Key]
+    public int Id { get; set; }
+    public int DeviceId { get; set; }
+    [ForeignKey("DeviceId")]
+    public Device Device { get; set; }
+    //public int UserId { get; set; }
+    public string Type { get; set; }
+    public DateTime Timestamp { get; set; }
+}

9b9c87a7254ab661aa640f56980e6d2050d17e77
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2023-09-23 11:54:24 +0600

    feat: Add ApiResponse model for track the responses from all the controller

---
+++ Homee.Models/Utils/ApiResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homee.Models.Utils;
+
+public class ApiResponse
+{
+    public ApiResponse()
+    {
+        ErrorMessages = new List<string>();
+    }
+    public HttpStatusCode StatusCode { get; set; }
+    public bool IsSuccess { get; set; } = true;
+    public List<string> ErrorMessages { get; set; }
+    public object Result { get; set; }
+}
